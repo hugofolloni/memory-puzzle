9744535ee66fb409e6c9e739aa2371f8d2b68182
Hugo Folloni <hugofollogua07@gmail.com>
2021-07-08 02:22:12 -0300

    Added commentaries and improved formatting

--- Python/memory.py
+++ Python/memory.py
@@ -4,48 +4,50 @@ import os
 
 def startGame():
 
-    numbers = list(range(10))
+    numbers = list(range(10)) # Cria uma lista com os 10 primeiros números e reorganiza eles
 
     random.shuffle(numbers)
 
-    print('Memorize esta sequência de números: ')
-    print(numbers)
-    time.sleep(10)
+    numberstr = [str(number) for number in numbers] # Transforma os números em "palavras" para formatação
+    
+    print('Memorize esta sequência de números: ') 
+    print(numberstr)
+    time.sleep(10) # Mostra as números por 10 segundos e depois, além de limpar o terminal, mostra os asteriscos
     os.system('cls' if os.name == 'nt' else 'clear')
-    asteriscos = ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
+    asteriscos = ['*'] * 10
     print(asteriscos)
 
     def game():
         print('Caso queira recomeçar, digite "r"')
-        inputlist = input('Me diga a posição do número do seguinte modo: "local na lista" e "número" => ')
-        if inputlist == 'r':
+        inputlist = input('Me diga a posição do número do seguinte modo: "local na lista" e "número" => ') # Input para coletar a informação de localização e número que o usuário tentou
+        if inputlist == 'r': # Restart1 do jogo
             os.system('cls' if os.name == 'nt' else 'clear')
             startGame()
         else: 
-            splited = inputlist.split()
+            splited = inputlist.split() # Separa a string em duas partes, adicionando à array local e número
 
             def avoidBlank():
 
-                local = int(splited[0]) - 1
-                numberx = int(splited[1])
+                local = int(splited[0]) - 1 # Local indicado - 1, pois primeira posição = 0
+                numberx = int(splited[1]) # Número chutado
 
 
 
-                if numbers[local] == numberx:
+                if numbers[local] == numberx: # Se o número chutado for igual ao número correto da posição
                     os.system('cls' if os.name == 'nt' else 'clear')
-                    asteriscos[local] = numberx 
-                    if '*' in asteriscos:
+                    asteriscos[local] = str(numberx) # Substitui o asterisco pelo número
+                    if '*' in asteriscos: # Caso ainda possuam asteriscos, coletar novamente os dados
                         print(asteriscos)
                         game()
-                    else: 
+                    else:  # Caso nao possuam mais asteriscos, o jogo acabou e você venceu
                         print("YOU WON")
                         print('------------------------')
                         playAgain = input("Quer jogar novamente? Responda com 'y' ou 'n' ")
-                        if playAgain == 'y':
+                        if playAgain == 'y': # Restart2
                             startGame()
                         else:
                             exit()
-                else:
+                else: # Caso esteja errado, aviso de erro e Restart3
                     os.system('cls' if os.name == 'nt' else 'clear')
                     print("WRONG! YOU LOST!")
                     tryAgain = input("Tentar novamente? Responda com 'y' ou 'n' ")
@@ -55,7 +57,7 @@ def startGame():
                     if tryAgain == 'n': 
                         exit()    
             
-            if len(splited) < 2:
+            if len(splited) < 2: # Caso ocorra erro de digutação e não separe por ' ' os números, computar erro e reinicar
                 os.system('cls' if os.name == 'nt' else 'clear')
                 print("WRONG! YOU LOST!")
                 tryAgain = input("Tentar novamente? Responda com 'y' ou 'n' ")
@@ -68,6 +70,6 @@ def startGame():
             else: 
                 avoidBlank()
 
-    game()
+    game() # Começar coleta de dados
 
-startGame()
+startGame() # Começar jogo
